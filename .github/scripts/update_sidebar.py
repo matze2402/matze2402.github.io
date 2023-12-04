af89cffa8d52d8f8930f2d46f4ecc18f38792d5a
matze2402 <145361419+matze2402@users.noreply.github.com>
2023-12-04 09:21:43 +0100

    Update update_sidebar.py

--- .github/scripts/update_sidebar.py
+++ .github/scripts/update_sidebar.py
@@ -1,13 +1,8 @@
 import os
 import json
 
-folder_path = import os
-
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
-
-sidebar_path = "matze2402/matze2402.github.io/blob/main/_data/sidebars/home_sidebar.yml"
+folder_path = "/matze2402/matze2402.github.io/tree/main/pages/mydoc"
+sidebar_path = "/matze2402/matze2402.github.io/blob/main/_data/sidebars/home_sidebar.yml"
 
 # Get a list of files in the folder
 files = os.listdir(folder_path)
