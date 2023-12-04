ff70f6e436937c372e902e3bb727bdefb63de686
matze2402 <145361419+matze2402@users.noreply.github.com>
2023-12-04 09:13:42 +0100

    Create update_sidebar.py

---
+++ .github/scripts/update_sidebar.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+folder_path = "matze2402/matze2402.github.io/tree/main/pages/mydoc"
+sidebar_path = "matze2402/matze2402.github.io/blob/main/_data/sidebars/home_sidebar.yml"
+
+# Get a list of files in the folder
+files = os.listdir(folder_path)
+
+# Update the sidebar configuration
+with open(sidebar_path, 'r') as f:
+    sidebar = json.load(f)
+
+# Modify the sidebar based on the new files
+for file in files:
+    sidebar["sections"]["New Files"].append({"text": file, "url": f"/path/to/folder/{file}"})
+
+# Save the updated sidebar
+with open(sidebar_path, 'w') as f:
+    json.dump(sidebar, f, indent=2)
