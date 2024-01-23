880db794d455a1e9447a2cecee5d6311634386fa
matze2402 <145361419+matze2402@users.noreply.github.com>
2024-01-23 12:54:07 +0100

    Update update_sidebar.py

--- .github/scripts/update_sidebar.py
+++ .github/scripts/update_sidebar.py
@@ -1,6 +1,6 @@
 import os
-#import shutil
-import yaml
+import shutil
+
 
 folder_path = "./pages/Testpurpose"
 sidebar_path = "./_data/sidebars/mydoc_sidebar.yml"
@@ -22,31 +22,31 @@ with open(sidebar_path, 'w') as f:
     yaml.dump(sidebar, f, indent=2)
     
     
-#     def move_files(source_folder, destination_folder):
-#     # Check if the source folder exists
-#     if not os.path.exists(source_folder):
-#         print(f"Source folder '{source_folder}' does not exist.")
-#         return
+    def move_files(source_folder, destination_folder):
+    # Check if the source folder exists
+    if not os.path.exists(source_folder):
+        print(f"Source folder '{source_folder}' does not exist.")
+        return
 
-#     # Check if the destination folder exists, create it if not
-#     if not os.path.exists(destination_folder):
-#         os.makedirs(destination_folder)
+    # Check if the destination folder exists, create it if not
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
 
-#     # Get a list of all files in the source folder
-#     files = os.listdir(source_folder)
+    # Get a list of all files in the source folder
+    files = os.listdir(source_folder)
 
-#     # Move each file to the destination folder
-#     for file in files:
-#         source_path = os.path.join(source_folder, file)
-#         destination_path = os.path.join(destination_folder, file)
-#         shutil.move(source_path, destination_path)
-#         print(f"Moved: {file}")
+    # Move each file to the destination folder
+    for file in files:
+        source_path = os.path.join(source_folder, file)
+        destination_path = os.path.join(destination_folder, file)
+        shutil.move(source_path, destination_path)
+        print(f"Moved: {file}")
 
-# # # Example usage:
-# source_folder  =  "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testpurpose"
-# destination_folder = "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testordner_neu"
+# # Example usage:
+source_folder  =  "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testpurpose"
+destination_folder = "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testordner_neu"
 
-# move_files(source_folder, destination_folder)
+move_files(source_folder, destination_folder)
 
 
 
