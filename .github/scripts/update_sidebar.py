f989b4270319c8cfddaf3b6a065d52d2b4f50e9b
matze2402 <145361419+matze2402@users.noreply.github.com>
2024-01-23 12:51:05 +0100

    Update update_sidebar.py

--- .github/scripts/update_sidebar.py
+++ .github/scripts/update_sidebar.py
@@ -1,5 +1,5 @@
 import os
-import shutil
+#import shutil
 import yaml
 
 folder_path = "./pages/Testpurpose"
@@ -14,41 +14,42 @@ with open(sidebar_path, 'r') as f:
 
 a = sidebar['entries'][0]['folders'][0]['folderitems']
 for file in files:
-    z = {"title": file , "url": f"/{file}"}   #erstellen eines neuen dicts in der richtigen Formatierung
+    z = {"title": file , "url": f"/{file}"}   
 
-a.append(z) # eine neues dict wird damit hinzugefügt
+a.append(z) 
 
 with open(sidebar_path, 'w') as f:
     yaml.dump(sidebar, f, indent=2)
+    
+    
+#     def move_files(source_folder, destination_folder):
+#     # Check if the source folder exists
+#     if not os.path.exists(source_folder):
+#         print(f"Source folder '{source_folder}' does not exist.")
+#         return
 
-def move_files(source_folder, destination_folder):
-    # Check if the source folder exists
-    if not os.path.exists(source_folder):
-        print(f"Source folder '{source_folder}' does not exist.")
-        return
+#     # Check if the destination folder exists, create it if not
+#     if not os.path.exists(destination_folder):
+#         os.makedirs(destination_folder)
 
-    # Check if the destination folder exists, create it if not
-    if not os.path.exists(destination_folder):
-        os.makedirs(destination_folder)
+#     # Get a list of all files in the source folder
+#     files = os.listdir(source_folder)
 
-    # Get a list of all files in the source folder
-    files = os.listdir(source_folder)
+#     # Move each file to the destination folder
+#     for file in files:
+#         source_path = os.path.join(source_folder, file)
+#         destination_path = os.path.join(destination_folder, file)
+#         shutil.move(source_path, destination_path)
+#         print(f"Moved: {file}")
 
-    # Move each file to the destination folder
-    for file in files:
-        source_path = os.path.join(source_folder, file)
-        destination_path = os.path.join(destination_folder, file)
-        shutil.move(source_path, destination_path)
-        print(f"Moved: {file}")
+# # # Example usage:
+# source_folder  =  "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testpurpose"
+# destination_folder = "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testordner_neu"
 
-# # Example usage:
-source_folder = "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testpurpose"
-destination_folder = "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testordner_neu"
-
-move_files(source_folder, destination_folder)
-
-
-
+# move_files(source_folder, destination_folder)
 
 
-# 8
+
+
+
+
