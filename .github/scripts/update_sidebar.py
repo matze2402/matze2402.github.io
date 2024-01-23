56ce87a9390d603532f214af003aec3e8b51aa2b
matze2402 <145361419+matze2402@users.noreply.github.com>
2024-01-23 12:00:23 +0100

    Update update_sidebar.py

--- .github/scripts/update_sidebar.py
+++ .github/scripts/update_sidebar.py
@@ -21,6 +21,32 @@ a.append(z) # eine neues dict wird damit hinzugefügt
 with open(sidebar_path, 'w') as f:
     json.dump(sidebar, f, indent=2)
 
+def move_files(source_folder, destination_folder):
+    # Check if the source folder exists
+    if not os.path.exists(source_folder):
+        print(f"Source folder '{source_folder}' does not exist.")
+        return
+
+    # Check if the destination folder exists, create it if not
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    # Get a list of all files in the source folder
+    files = os.listdir(source_folder)
+
+    # Move each file to the destination folder
+    for file in files:
+        source_path = os.path.join(source_folder, file)
+        destination_path = os.path.join(destination_folder, file)
+        shutil.move(source_path, destination_path)
+        print(f"Moved: {file}")
+
+# # Example usage:
+source_folder = "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testpurpose"
+destination_folder = "C:/Users/smmagack/Desktop/documentation-theme-jekyll-gh-pages/pages/Testordner_neu"
+
+move_files(source_folder, destination_folder)
+
 
 
 
