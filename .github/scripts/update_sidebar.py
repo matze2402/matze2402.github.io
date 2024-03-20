fa2f7850d2a7ddb4a4e35bd4c1f81035e9053b98
matze2402 <145361419+matze2402@users.noreply.github.com>
2024-03-20 07:38:36 +0100

    Update update_sidebar.py

--- .github/scripts/update_sidebar.py
+++ .github/scripts/update_sidebar.py
@@ -7,21 +7,15 @@ folder_path = './pages/Testpurpose'  # Update this with your folder path
 # Path to the YAML file where the sidebar content will be written
 sidebar_file_path = './_data/sidebars/mydoc_sidebar.yml'  # Update this with your desired sidebar file path
 
-def generate_sidebar():
-    # Fetch the list of files in the folder
-    files = os.listdir(folder_path)
+import os
+files = [f for f in os.listdir('folder_patj') if os.path.isfile(f)]
 
-    # Filter out directories, if any
-    files = [f for f in files if os.path.isfile(os.path.join(folder_path, f))]
+sidebar_file = open('sidebar_file_path', 'w')
 
-    # Convert the list of files to a dictionary suitable for YAML
-    sidebar_content = [{'title': file, 'url': file} for file in files]
+for file in files:
+	if ".md" in file:
+		name = file.split(".md")
+		file = file.replace(" ", "%20")
+		sidebar_file.write( f"* [{name[0]}]({file})\n" )
 
-    # Write the sidebar content to the YAML file
-    with open(sidebar_file_path, 'w') as sidebar_file:
-        yaml.dump(sidebar_content, sidebar_file, default_flow_style=False)
-
-    print(f"Sidebar content generated and saved to '{sidebar_file_path}'")
-
-if __name__ == "__main__":
-    generate_sidebar()
+sidebar_file.close()
